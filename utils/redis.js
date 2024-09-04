9cdabddff93792f894400e6a90bdaf362ef4f6f9
lepronet <lepronet85@gmail.com>
2024-09-04 09:58:04 +0100

    Redis connection

--- utils/redis.js
+++ utils/redis.js
@@ -1,59 +1,29 @@
-import redis from "redis";
+import { createClient } from "redis";
 import { promisify } from "util";
 
-/**
- * Class for performing operations with Redis service
- */
 class RedisClient {
   constructor() {
-    this.client = redis.createClient();
-    this.getAsync = promisify(this.client.get).bind(this.client);
-
-    this.client.on("error", (error) => {
-      console.log(`Redis client not connected to the server: ${error.message}`);
-    });
-
-    this.client.on("connect", () => {
-      // console.log('Redis client connected to the server');
-    });
+    this.myClient = createClient();
+    this.myClient.on("error", (error) => console.log(error));
   }
 
-  /**
-   * Checks if connection to Redis is Alive
-   * @return {boolean} true if connection alive or false if not
-   */
   isAlive() {
-    return this.client.connected;
+    return this.myClient.connected;
   }
 
-  /**
-   * gets value corresponding to key in redis
-   * @key {string} key to search for in redis
-   * @return {string}  value of key
-   */
   async get(key) {
-    const value = await this.getAsync(key);
-    return value;
+    const getAsync = promisify(this.myClient.GET).bind(this.myClient);
+    return getAsync(key);
   }
 
-  /**
-   * Creates a new key in redis with a specific TTL
-   * @key {string} key to be saved in redis
-   * @value {string} value to be asigned to key
-   * @duration {number} TTL of key
-   * @return {undefined}  No return
-   */
-  async set(key, value, duration) {
-    this.client.setex(key, duration, value);
+  async set(key, val, time) {
+    const setAsync = promisify(this.myClient.SET).bind(this.myClient);
+    return setAsync(key, val, "EX", time);
   }
 
-  /**
-   * Deletes key in redis service
-   * @key {string} key to be deleted
-   * @return {undefined}  No return
-   */
   async del(key) {
-    this.client.del(key);
+    const delAsync = promisify(this.myClient.DEL).bind(this.myClient);
+    return delAsync(key);
   }
 }
 
